5c288c748c2fa2d147d5343a33771a7c1dff79bb
Gaurav Tyagi <gtyagi88cse@gmail.com>
2019-07-02 13:54:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener {
+
+            if(TextUtils.isEmpty(etFirstNum.text.toString()) || TextUtils.isEmpty(etSecondNum.text.toString())) {
+
+                Toast.makeText(this, "Invalid Input",LENGTH_SHORT).show()
+
+            }
+            else {
+
+               var firstNum = etFirstNum.text.toString().toInt()
+               var secondNum = etSecondNum.text.toString().toInt()
+               var stringOfSum = firstNum.toString() + " + " + secondNum.toString() + " = " + (firstNum + secondNum).toString()
+
+               val intent = Intent(this, Main2Activity::class.java)
+               intent.putExtra("EQN", stringOfSum)
+               startActivity(intent)
+
+            }
+        }
+
+    }
+}
